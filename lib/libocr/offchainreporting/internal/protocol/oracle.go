5c716782679dab335f4c00bf8de2b5b877119f0b
alanxiaoy2 <664962185@qq.com>
2021-12-23 17:03:58 +0800

    fix offchain transmit

--- lib/libocr/offchainreporting/internal/protocol/oracle.go
+++ lib/libocr/offchainreporting/internal/protocol/oracle.go
@@ -169,13 +169,9 @@ func (o *oracleState) run() {
 	for {
 		select {
 		case msg := <-chNet:
-			if o.shouldRun(){
-				msg.Msg.process(o, msg.Sender)
-			}
+			msg.Msg.process(o, msg.Sender)
 		case epoch := <-chPacemakerToOracle:
-			if o.shouldRun(){
-				o.epochChanged(epoch)
-			}
+			o.epochChanged(epoch)
 		case <-chDone:
 		}
 
@@ -260,33 +256,33 @@ func (o *oracleState) reportGenerationMessage(msg MessageToReportGeneration, sen
 }
 
 
-//judge whether the OracleID is in newIndexes or not.If not,it shouldn't run.
-func (o *oracleState) shouldRun() bool {
-	var resultNewIndexes struct {
-		newIndexes []int
-		err          error
-	}
-	ok := o.subprocesses.BlockForAtMost(o.ctx, o.localConfig.BlockchainTimeout,
-		func(ctx context.Context) {
-			resultNewIndexes.newIndexes, resultNewIndexes.err =
-				o.contractTransmitter.LatestNewIndexes(
-					ctx,
-					o.Config.DeltaC,
-				)
-		},
-	)
-	if !ok {
-		o.logger.Error("oracleState shouldRun: blockchain interaction timed out, returning true", types.LogFields{
-			"id":  				 o.id,
-			"timeout":           o.localConfig.BlockchainTimeout,
-		})
-		return true
-	}
-	if IsExist(resultNewIndexes.newIndexes,int(o.id)){
-		return true
-	}
-	return false
-}
+////judge whether the OracleID is in newIndexes or not.If not,it shouldn't run.
+//func (o *oracleState) shouldRun() bool {
+//	var resultNewIndexes struct {
+//		newIndexes []int
+//		err          error
+//	}
+//	ok := o.subprocesses.BlockForAtMost(o.ctx, o.localConfig.BlockchainTimeout,
+//		func(ctx context.Context) {
+//			resultNewIndexes.newIndexes, resultNewIndexes.err =
+//				o.contractTransmitter.LatestNewIndexes(
+//					ctx,
+//					o.Config.DeltaC,
+//				)
+//		},
+//	)
+//	if !ok {
+//		o.logger.Error("oracleState shouldRun: blockchain interaction timed out, returning true", types.LogFields{
+//			"id":  				 o.id,
+//			"timeout":           o.localConfig.BlockchainTimeout,
+//		})
+//		return true
+//	}
+//	if IsExist(resultNewIndexes.newIndexes,int(o.id)){
+//		return true
+//	}
+//	return false
+//}
 
 func IsExist(array []int,a int)bool {
 	if len(array)==0{
--- lib/libocr/offchainreporting/internal/protocol/transmission.go
+++ lib/libocr/offchainreporting/internal/protocol/transmission.go
@@ -442,30 +442,30 @@ func (t *transmissionState) transmitDelay(epoch uint32, round uint8) *time.Durat
 	return nil
 }
 
-//judge whether the OracleID is in newIndexes or not.If not,it shouldn't run.
-func (t *transmissionState) shouldRun() bool {
-	var resultNewIndexes struct {
-		newIndexes []int
-		err          error
-	}
-	ok := t.subprocesses.BlockForAtMost(t.ctx, t.localConfig.BlockchainTimeout,
-		func(ctx context.Context) {
-			resultNewIndexes.newIndexes, resultNewIndexes.err =
-				t.transmitter.LatestNewIndexes(
-					ctx,
-					t.config.DeltaC,
-				)
-		},
-	)
-	if !ok {
-		t.logger.Error("transmissionState shouldRun: blockchain interaction timed out, returning true", types.LogFields{
-			"latestEpochRound":  t.latestEpochRound,
-			"timeout":           t.localConfig.BlockchainTimeout,
-		})
-		return true
-	}
-	if IsExist(resultNewIndexes.newIndexes,int(t.id)){
-		return true
-	}
-	return false
-}
+////judge whether the OracleID is in newIndexes or not.If not,it shouldn't run.
+//func (t *transmissionState) shouldRun() bool {
+//	var resultNewIndexes struct {
+//		newIndexes []int
+//		err          error
+//	}
+//	ok := t.subprocesses.BlockForAtMost(t.ctx, t.localConfig.BlockchainTimeout,
+//		func(ctx context.Context) {
+//			resultNewIndexes.newIndexes, resultNewIndexes.err =
+//				t.transmitter.LatestNewIndexes(
+//					ctx,
+//					t.config.DeltaC,
+//				)
+//		},
+//	)
+//	if !ok {
+//		t.logger.Error("transmissionState shouldRun: blockchain interaction timed out, returning true", types.LogFields{
+//			"latestEpochRound":  t.latestEpochRound,
+//			"timeout":           t.localConfig.BlockchainTimeout,
+//		})
+//		return true
+//	}
+//	if IsExist(resultNewIndexes.newIndexes,int(t.id)){
+//		return true
+//	}
+//	return false
+//}
